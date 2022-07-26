6e94b94257238f8f1eca8835d52f9519a2af3457
YimingXu1 <yiming.tsui@gmail.com>
2022-07-26 13:18:55 +0200

    Update build_wav_files.py

--- utils/build_wav_files.py
+++ utils/build_wav_files.py
@@ -1,5 +1,19 @@
 import os
+from shutil import copyfile
 
-file_ads = './../data/Flickr8k/audio/wavfile/wavs'
-out_ads = './../data/Flickr8k/audio/wavs'
+file_ads = './../data/Flickr8k/audio/wavs'
+out_ads = './../data/Flickr8k/audio2/wavs'
+
+def list_wavs(file_ads, out_ads):
+    audio_names = os.listdir(file_ads)
+
+    for audio_name in sorted(audio_names):
+        class_name = audio_name[:-6]
+        source_path = os.path.join(file_ads, audio_name)
+        target_path = os.path.join(out_ads, class_name, audio_name)
+        if not os.path.exists(os.path.join(out_ads, class_name)):
+            os.makedirs(os.path.join(out_ads, class_name))
+        copyfile(source_path, target_path)
+
+#list_wavs(file_ads, out_ads)
 
