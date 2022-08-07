34622662bf59101f3892e2fbd66eb70dad9b6fbf
YimingXu1 <yiming.tsui@gmail.com>
2022-08-07 23:00:05 +0200

    Update text2audio.py

--- utils/text2audio.py
+++ utils/text2audio.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 
 import torch
 import torchaudio
+import os
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 matplotlib.rcParams["figure.figsize"] = [16.0, 4.8]
 
@@ -20,18 +23,21 @@ processor = bundle.get_text_processor()
 tacotron2 = bundle.get_tacotron2().to(device)
 vocoder = bundle.get_vocoder().to(device)
 
-text = "Hello world! Text to speech!"
+path = './../data/FFHQ-Text/Text/00_Female'
+img_names = os.listdir(path)
 
-with torch.inference_mode():
-    processed, lengths = processor(text)
-    processed = processed.to(device)
-    lengths = lengths.to(device)
-    spec, spec_lengths, _ = tacotron2.infer(processed, lengths)
-    waveforms, lengths = vocoder(spec, spec_lengths)
-
-fig, [ax1, ax2] = plt.subplots(2, 1, figsize=(16, 9))
-ax1.imshow(spec[0].cpu().detach())
-ax2.plot(waveforms[0].cpu().detach())
-
-torchaudio.save("_assets/output_wavernn.wav", waveforms[0:1].cpu(), sample_rate=vocoder.sample_rate)
-IPython.display.Audio("_assets/output_wavernn.wav")
+texts = []
+for img_name in sorted(img_names):
+    print(img_name)
+    i = 0
+    for line in open(os.path.join(path , img_name)):
+        text = line.strip('\n')
+        with torch.inference_mode():
+            processed, lengths = processor(text)
+            processed = processed.to(device)
+            lengths = lengths.to(device)
+            spec, spec_lengths, _ = tacotron2.infer(processed, lengths)
+            waveforms, lengths = vocoder(spec, spec_lengths)
+        save_path = './../data/FFHQ-Text/audio' + '/'+ img_name[:-4] + '_' + str(i) + '.wav'
+        torchaudio.save(save_path, waveforms[0:1].cpu(), sample_rate=vocoder.sample_rate)
+        i = i+1
