8be62984511c2e5ff18704685d65e30470157817
YimingXu1 <yiming.tsui@gmail.com>
2022-08-08 23:57:28 +0200

    Update AudioModels.py

--- model/AudioModels.py
+++ model/AudioModels.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+import math
 from options import cfg
 
 print(cfg.CNNRNN_RNN.input_size)
@@ -20,6 +21,60 @@ class multi_attention(nn.Module):
             self.alpha.append(head.alpha)
         # return the resulting embedding 
         return torch.cat(out, 1)
+
+def scaled_dot_product(q, k, v, mask=None):
+    d_k = q.size()[-1]
+    attn_logits = torch.matmul(q, k.transpose(-2, -1))
+    attn_logits = attn_logits / math.sqrt(d_k)
+    if mask is not None:
+        attn_logits = attn_logits.masked_fill(mask == 0, -9e15)
+    attention = F.softmax(attn_logits, dim=-1)
+    values = torch.matmul(attention, v)
+    return values, attention
+
+class MultiheadAttention(nn.Module):
+
+    def __init__(self, input_dim, embed_dim, num_heads):
+        super().__init__()
+        assert embed_dim % num_heads == 0, "Embedding dimension must be 0 modulo number of heads."
+
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        self.head_dim = embed_dim // num_heads
+
+        # Stack all weight matrices 1...h together for efficiency
+        # Note that in many implementations you see "bias=False" which is optional
+        self.qkv_proj = nn.Linear(input_dim, 3*embed_dim)
+        self.o_proj = nn.Linear(embed_dim, embed_dim)
+
+        self._reset_parameters()
+
+    def _reset_parameters(self):
+        # Original Transformer initialization, see PyTorch documentation
+        nn.init.xavier_uniform_(self.qkv_proj.weight)
+        self.qkv_proj.bias.data.fill_(0)
+        nn.init.xavier_uniform_(self.o_proj.weight)
+        self.o_proj.bias.data.fill_(0)
+
+    def forward(self, x, mask=None, return_attention=False):
+        batch_size, seq_length, embed_dim = x.size()
+        qkv = self.qkv_proj(x)
+
+        # Separate Q, K, V from linear output
+        qkv = qkv.reshape(batch_size, seq_length, self.num_heads, 3*self.head_dim)
+        qkv = qkv.permute(0, 2, 1, 3) # [Batch, Head, SeqLen, Dims]
+        q, k, v = qkv.chunk(3, dim=-1)
+
+        # Determine value outputs
+        values, attention = scaled_dot_product(q, k, v, mask=mask)
+        values = values.permute(0, 2, 1, 3) # [Batch, SeqLen, Head, Dims]
+        values = values.reshape(batch_size, seq_length, embed_dim)
+        o = self.o_proj(values)
+
+        if return_attention:
+            return o, attention
+        else:
+            return o
 
 class attention(nn.Module):
     def __init__(self, in_size, hidden_size):
@@ -58,7 +113,7 @@ class CNN_RNN_ENCODER(nn.Module):
         else:
             raise NotImplementedError
 
-        self.att = multi_attention(in_size = cfg.CNNRNN_ATT.in_size, hidden_size = cfg.CNNRNN_ATT.hidden_size, n_heads = cfg.CNNRNN_ATT.n_heads)
+        self.att = MultiheadAttention(input_dim = cfg.CNNRNN_ATT.in_size, embed_dim = cfg.CNNRNN_ATT.hidden_size, num_heads = cfg.CNNRNN_ATT.n_heads)
 
     def forward(self, input, l):
         input = input.transpose(2,1)
