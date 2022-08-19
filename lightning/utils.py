b3ef55da6192a266554996217b0546fa2eb48a7e
YimingXu1 <yiming.tsui@gmail.com>
2022-08-19 08:11:03 +0200

    Update utils.py

--- lightning/utils.py
+++ lightning/utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 from easydict import EasyDict as edict
+import torch
+import math
 from torch.utils.data.dataloader import default_collate
 
 __C = edict()
@@ -250,3 +252,48 @@ def pad_collate(batch):
         batch.sort(key=lambda x: x[-2], reverse=True)
 
     return default_collate(batch)
+
+
+def frame_signal(signal: torch.Tensor,
+                 frame_length: int,
+                 hop_length: int,
+                 window: torch.Tensor = None) -> torch.Tensor:
+
+    if window is None:
+        window = torch.ones(frame_length, dtype=signal.dtype, device=signal.device)
+
+    if window.shape[0] != frame_length:
+        raise ValueError('Wrong `window` length: expected {}, got {}'.format(window.shape[0], frame_length))
+
+    signal_length = signal.shape[-1]
+
+    if signal_length <= frame_length:
+        num_frames = 1
+    else:
+        num_frames = 1 + int(math.ceil((1.0 * signal_length - frame_length) / hop_length))
+
+    pad_len = int((num_frames - 1) * hop_length + frame_length)
+    if pad_len > signal_length:
+        zeros = torch.zeros(pad_len - signal_length, device=signal.device, dtype=signal.dtype)
+
+        while zeros.dim() < signal.dim():
+            zeros.unsqueeze_(0)
+
+        pad_signal = torch.cat((zeros.expand(*signal.shape[:-1], -1)[..., :zeros.shape[-1] // 2], signal), dim=-1)
+        pad_signal = torch.cat((pad_signal, zeros.expand(*signal.shape[:-1], -1)[..., zeros.shape[-1] // 2:]), dim=-1)
+    else:
+        pad_signal = signal
+
+    indices = torch.arange(0, frame_length, device=signal.device).repeat(num_frames, 1)
+    indices += torch.arange(
+        0,
+        num_frames * hop_length,
+        hop_length,
+        device=signal.device
+    ).repeat(frame_length, 1).t_()
+    indices = indices.long()
+
+    frames = pad_signal[..., indices]
+    frames = frames * window
+
+    return frames
